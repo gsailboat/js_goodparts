5b46c0dd95b85452b8d70f97ec116b16f5e07b98
Greg Selbo <gselbo@42.us.org>
2017-08-31 16:37:17 -0700

    Add new leetcode excerices:

--- leetcode/rev_string.js
+++ leetcode/rev_string.js
@@ -1,13 +1,14 @@
 var reverseWords = function(str) {
-    var s = str.split();
+    var s = str.split(" ");
     var st = "";
-    for (var i = s.length - 1; i > 0; i--) {
-        st += s[i];
-        if (i > 0)
-            st += " ";
+    for (var i = s.length - 1; i >= 0; i--) {
+    	if (s[i] != "") {
+            if (st != "" && st[st.length - 1] != " ")
+                st += " ";
+        	st += s[i];
+        	if (i > 0 && s[i - 1] != "")
+            	st += " ";
+        }
     }
-    // console.log(st);
     return st;
 };
-
-// reverseWords(process.argv2);
